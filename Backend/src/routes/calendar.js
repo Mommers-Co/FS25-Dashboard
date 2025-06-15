c0923e792d35f8746cb1ae34831f50a53aa6d935
Andrew <62702514+AndrewMommers@users.noreply.github.com>
2025-06-15 16:13:36 +1000

    reset

--- Backend/src/routes/calendar.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const calendarController = require("../controllers/calendarController");
-
-const router = express.Router();
-
-router.get("/", calendarController.getCalendarData);
-router.post("/", calendarController.addCalendarEvent);
-
-module.exports = router;
